20c01908792d1b080905ff09eb2839bb2a208c4a
Victor Manoel <victor1721556@gmail.com>
2022-04-03 13:09:02 -0300

    Criando a barra de menu.

--- Codificando/agenda.py
+++ Codificando/agenda.py
@@ -9,8 +9,12 @@ janela_principal.config(bg='black')
 janela_principal.title('Agenda Agora')
 janela_principal.iconbitmap('codificando/imagens/agenda.ico')
 
+# Importando imagens e apagando o fundo
+contato = PhotoImage(file="Codificando/imagens/adicionar_usuario.png")
+
+
 # Labels
-contatos = Label(janela_principal, text='Contatos',
+contatos_titulo = Label(janela_principal, text='Contatos',
                  fg='white',
                  bg='black',
                  font=("Righteous", 20)
@@ -23,11 +27,22 @@ caixa_pesquisa = Entry(janela_principal, font=('Times', 15),
                        relief="raised")
 caixa_pesquisa.insert(0, "Buscar...")
 
+# Botões
+contato_button = Button(janela_principal,
+                        image=contato,
+                        bg='black',
+                        width=25,
+                        height=25
+                        )
+
 # Rodando
-contatos.place(x=10,
-               y=5
-               )
-caixa_pesquisa.place(x=150,
+contatos_titulo.place(x=10,
+                      y=5
+                      )
+caixa_pesquisa.place(x=600,
                      y=12
+                     )
+contato_button.place(x=900,
+                     y=5
                      )
 janela_principal.mainloop()
