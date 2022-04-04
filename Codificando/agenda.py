78da12b79708a1490c0ce508e4d3b2db588c5b3b
Victor Manoel <victor1721556@gmail.com>
2022-04-04 07:13:42 -0300

    update1.3

--- Codificando/agenda.py
+++ Codificando/agenda.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 # Especificações da janela
 janela_principal = tk.Tk()
-janela_principal.geometry('960x540')
+janela_principal.geometry('850x540')
 janela_principal.resizable(0, 0)
 janela_principal.config(bg='black')
 janela_principal.title('Agenda Agora')
@@ -11,10 +11,11 @@ janela_principal.iconbitmap('codificando/imagens/agenda.ico')
 
 # Importando imagens e apagando o fundo
 contato = PhotoImage(file="Codificando/imagens/adicionar_usuario.png")
+lupa = PhotoImage(file="Codificando/imagens/pesquisa.png")
 
 
 # Labels
-contatos_titulo = Label(janela_principal, text='Contatos',
+contatos_titulo = Label(janela_principal, text='Agenda Pessoal de Contatos',
                  fg='white',
                  bg='black',
                  font=("Righteous", 20)
@@ -29,11 +30,17 @@ caixa_pesquisa.insert(0, "Buscar...")
 
 # Botões
 contato_button = Button(janela_principal,
+                        text='Adicionar',
                         image=contato,
                         bg='black',
-                        width=25,
                         height=25
                         )
+pesquisar_button = Button(janela_principal,
+                          image=lupa,
+                          bg='black',
+                          width=25,
+                          height=25
+                          )
 
 # Rodando
 contatos_titulo.place(x=10,
@@ -42,7 +49,10 @@ contatos_titulo.place(x=10,
 caixa_pesquisa.place(x=600,
                      y=12
                      )
-contato_button.place(x=900,
-                     y=5
+pesquisar_button.place(x=806,
+                       y=11
+                       )
+contato_button.place(x=200,
+                     y=60
                      )
 janela_principal.mainloop()
