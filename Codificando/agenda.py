2678b4cfa89d241357759e557793b8294b271af8
Victor Manoel <victor1721556@gmail.com>
2022-04-05 23:57:20 -0300

    update1.8

--- Codificando/agenda.py
+++ Codificando/agenda.py
@@ -4,8 +4,51 @@ from awesometkinter import *
 import awesometkinter as atk
 from tkinter import ttk
 
+class funcoes():
+    def limpar_caixa(self):
+        def on_click(event):
+            self.caixa_pesquisa.configure(state=NORMAL)
+            self.caixa_pesquisa.delete(0, END)
 
-class Application():
+            self.caixa_pesquisa.unbind('<Button-1>', on_click)
+        self.caixa_pesquisa.delete(0, END)
+        self.caixa_pesquisa.insert(0, "Buscar...")
+        self.caixa_pesquisa.configure(state=DISABLED)
+        on_click_id = self.caixa_pesquisa.bind('<Button-1>', on_click)
+
+        self.nome_entry.delete(0, END)
+
+        def on_click(event):
+            self.primeiro_numero.configure(state=NORMAL)
+            self.primeiro_numero.delete(0, END)
+
+            self.primeiro_numero.unbind('<Button-1>', on_click)
+        self.primeiro_numero.delete(0, END)
+        self.primeiro_numero.insert(0, "Número 1")
+        self.primeiro_numero.configure(state=DISABLED)
+        on_click_id_2 = self.primeiro_numero.bind('<Button-1>', on_click)
+
+        def on_click(event):
+            self.segundo_numero.configure(state=NORMAL)
+            self.segundo_numero.delete(0, END)
+
+            self.segundo_numero.unbind('<Button-1>', on_click)
+        self.segundo_numero.delete(0, END)
+        self.segundo_numero.insert(0, "Número 2")
+        self.segundo_numero.configure(state=DISABLED)
+        on_click_id_3 = self.segundo_numero.bind('<Button-1>', on_click)
+
+        def on_click(event):
+            self.terceiro_numero.configure(state=NORMAL)
+            self.terceiro_numero.delete(0, END)
+
+            self.terceiro_numero.unbind('<Button-1>', on_click)
+        self.terceiro_numero.delete(0, END)
+        self.terceiro_numero.insert(0, "Número 3")
+        self.terceiro_numero.configure(state=DISABLED)
+        on_click_id_4 = self.terceiro_numero.bind('<Button-1>', on_click)
+
+class Application(funcoes):
     def __init__(self):
         self.janela_principal = Tk()
         self.imagens()
@@ -92,8 +135,8 @@ class Application():
         # Caixa de pesquisa da barra de menu
         self.caixa_pesquisa = Entry(self.janela_principal,
                 font=('Times', 15),
-                fg='gray',
-                borderwidth=3,
+                fg='#000000',
+                borderwidth=2,
                 relief="raised"
                 )
         self.caixa_pesquisa.insert(0, "Buscar...")
@@ -101,28 +144,66 @@ class Application():
         on_click_id = self.caixa_pesquisa.bind('<Button-1>', on_click)
 
         # Caixa de texto que recebe o nome
+        def on_click(event):
+            self.nome_entry.configure(state=NORMAL)
+            self.nome_entry.delete(0, END)
+
+            self.nome_entry.unbind('<Button-1>', on_click)
         self.nome_entry = Entry(self.primeiro_frame,
                 font=('Times', 15),
-                fg='gray',
+                fg='#000000',
                 borderwidth=2,
                 relief='raised')
 
         # Adicionando as três caixas de texto que receberão os números de telefone
+
+        ### RECEBE O PRIMEIRO NUMERO ###
+        def on_click(event):
+            self.primeiro_numero.configure(state=NORMAL)
+            self.primeiro_numero.delete(0, END)
+
+            self.primeiro_numero.unbind('<Button-1>', on_click)
+
         self.primeiro_numero = Entry(self.primeiro_frame,
                 font=('Times', 15),
-                fg='gray',
+                fg='#000000',
                 borderwidth=2,
                 relief='raised')
+        self.primeiro_numero.insert(0, "Número 1")
+        self.primeiro_numero.configure(state=DISABLED)
+        on_click_id_2 = self.primeiro_numero.bind('<Button-1>', on_click)
+
+        ### RECEBE O SEGUNDO NUMERO ###
+        def on_click(event):
+            self.segundo_numero.configure(state=NORMAL)
+            self.segundo_numero.delete(0, END)
+
+            self.segundo_numero.unbind('<Button-1>', on_click)
+
         self.segundo_numero = Entry(self.primeiro_frame,
                 font=('Times', 15),
-                fg='gray',
+                fg='#000000',
                 borderwidth=2,
                 relief='raised')
+        self.segundo_numero.insert(0, "Número 2")
+        self.segundo_numero.configure(state=DISABLED)
+        on_click_id_3 = self.segundo_numero.bind('<Button-1>', on_click)
+
+        ### RECEBE O TERCEIRO NUMERO ###
+        def on_click(event):
+            self.terceiro_numero.configure(state=NORMAL)
+            self.terceiro_numero.delete(0, END)
+
+            self.terceiro_numero.unbind('<Button-1>', on_click)
+
         self.terceiro_numero = Entry(self.primeiro_frame,
                 font=('Times', 15),
-                fg='gray',
+                fg='#000000',
                 borderwidth=2,
                 relief='raised')
+        self.terceiro_numero.insert(0, "Número 3")
+        self.terceiro_numero.configure(state=DISABLED)
+        on_click_id_4 = self.terceiro_numero.bind('<Button-1>', on_click)
 
         # Chamando as caxas de texto
         self.caixa_pesquisa.place(x=600, y=12)
@@ -138,24 +219,21 @@ class Application():
                 image=self.lupa,
                 bg='#2F4F4F',
                 width=25,
-                height=25
-                )
+                height=25)
 
         # Botão de adiconar novo contato
-        self.adicionar_button = Button(self.primeiro_frame,
+        self.salvar_button = Button(self.primeiro_frame,
                 text='Salvar',
                 font=('verdana', 10, 'bold'),
                 fg='white',
-                bg='#5271FF'
-                )
+                bg='#5271FF')
 
         # Botão de editar contato
-        self.alterar_button = Button(self.primeiro_frame,
+        self.editar_button = Button(self.primeiro_frame,
                 text='Editar',
                 font=('verdana', 10, 'bold'),
                 fg='white',
-                bg='#5271FF'
-                )
+                bg='#5271FF')
 
         # Botão remover contatos
         self.remover_button = Button(self.segundo_frame,
@@ -163,12 +241,30 @@ class Application():
                 font=('verdana', 10, 'bold'),
                 fg='white',
                 bg='#FF5757')
+
+        # Botão para sair da janela
+        self.sair_button = Button(self.primeiro_frame,
+                command=quit,
+                text='Sair',
+                font=('verdana', 10, 'bold'),
+                fg='white',
+                bg='#FF5757')
+
+        # Botão para limpar as caixas de texto
+        self.limpar_button = Button(self.primeiro_frame,
+                command=self.limpar_caixa,
+                text='Limpar',
+                font=('verdana', 10, 'bold'),
+                fg='white',
+                bg='#FF5757')
         
         # Chamando os botões na tela
         self.pesquisar_button.place(x=805, y=11)
-        self.adicionar_button.place(x=330, y=170)
-        self.alterar_button.place(x=440, y=170)
-        self.remover_button.place(x=730, y=15)
+        self.salvar_button.place(x=330, y=170)
+        self.editar_button.place(x=440, y=170)
+        self.remover_button.place(x=735, y=15)
+        self.sair_button.place(x=770, y=190)
+        self.limpar_button.place(x=750, y=20)
 
 
     def tabela_de_contatos(self):
@@ -189,8 +285,13 @@ class Application():
         self.tabela.column('#2', width=205)
         self.tabela.column('#3', width=205)
 
+        # Criando a ScrollBar
+        self.barra_de_rolagem = SimpleScrollbar(self.segundo_frame,
+                orient=VERTICAL)
+        self.tabela.configure(yscroll=self.barra_de_rolagem.set)
+
         # Especificando o tamanho no frame
-        self.tabela.place(x=10, y=50, width=810, height=180)
-        
+        self.tabela.place(x=10, y=50, width=790, height=180)
+        self.barra_de_rolagem.place(x=799, y=50, width=17, height=180)
 
 Application()
