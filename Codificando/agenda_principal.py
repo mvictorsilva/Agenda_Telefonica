28eb81a911882fe309253232ff0908ce4224d8ba
Victor Manoel <victor1721556@gmail.com>
2022-04-07 17:35:49 -0300

    update2.1
    Criando função para o botão de apagar.
    E algumas outras alterações no código.

--- Codificando/agenda_principal.py
+++ Codificando/agenda_principal.py
@@ -6,60 +6,36 @@ from tkinter import ttk
 import sqlite3
 
 class funcoes():
+    # Função que limpa as caixas de entrada
     def limpar_caixa(self):
-        def on_click(event):
-            self.caixa_pesquisa.configure(state=NORMAL)
-            self.caixa_pesquisa.delete(0, END)
-
-            self.caixa_pesquisa.unbind('<Button-1>', on_click)
         self.caixa_pesquisa.delete(0, END)
-        self.caixa_pesquisa.insert(0, "Buscar...")
-        self.caixa_pesquisa.configure(state=DISABLED)
-        on_click_id = self.caixa_pesquisa.bind('<Button-1>', on_click)
+
+        self.codigo_entry.delete(0, END)
 
         self.nome_entry.delete(0, END)
 
-        def on_click(event):
-            self.primeiro_numero.configure(state=NORMAL)
-            self.primeiro_numero.delete(0, END)
-
-            self.primeiro_numero.unbind('<Button-1>', on_click)
         self.primeiro_numero.delete(0, END)
-        self.primeiro_numero.insert(0, "Número 1")
-        self.primeiro_numero.configure(state=DISABLED)
-        on_click_id_2 = self.primeiro_numero.bind('<Button-1>', on_click)
 
-        def on_click(event):
-            self.segundo_numero.configure(state=NORMAL)
-            self.segundo_numero.delete(0, END)
-
-            self.segundo_numero.unbind('<Button-1>', on_click)
         self.segundo_numero.delete(0, END)
-        self.segundo_numero.insert(0, "Número 2")
-        self.segundo_numero.configure(state=DISABLED)
-        on_click_id_3 = self.segundo_numero.bind('<Button-1>', on_click)
 
-        def on_click(event):
-            self.terceiro_numero.configure(state=NORMAL)
-            self.terceiro_numero.delete(0, END)
-
-            self.terceiro_numero.unbind('<Button-1>', on_click)
         self.terceiro_numero.delete(0, END)
-        self.terceiro_numero.insert(0, "Número 3")
-        self.terceiro_numero.configure(state=DISABLED)
-        on_click_id_4 = self.terceiro_numero.bind('<Button-1>', on_click)
 
+    # Função que cria e conecta ao banco de dados no computador
     def conectar_ao_bd(self):
             self.conectar = sqlite3.connect('cliente.db')
             self.cursor = self.conectar.cursor()
             print('Conectando ao banco de dados')
 
+    # Função que desconecta o banco de dados depois que cada processo acaba
     def desconectar_ao_bd(self):
             self.conectar.close()
             print('Desconectando ao banco de dados')
 
+    # Criando a tabela de dados no banco de dados
     def criar_tabela(self):
             self.conectar_ao_bd()
+
+            # Essa função so cria uma tabela se ela não existir
             self.cursor.execute("""
                 create table if not exists contato (
                         id integer not null primary key,
@@ -73,12 +49,17 @@ class funcoes():
             print('Banco de dados criado')
             self.desconectar_ao_bd()
 
-    def adicionar_cliente(self):
+    # Criando as variaveis que receberão os dados das caixas de texto (entry)
+    def variaveis(self):
             self.codigo_id = self.codigo_entry.get()
             self.inserir_nome_ctt = self.nome_entry.get()
             self.inserir_num_um = self.primeiro_numero.get()
             self.inserir_num_dois = self.segundo_numero.get()
             self.inserir_num_tres = self.terceiro_numero.get()
+
+    # Função que cria inseri os dados recebidos das variaveis pelo usuário
+    def adicionar_cliente(self):
+            self.variaveis()
             self.conectar_ao_bd()
 
             self.cursor.execute("""
@@ -94,22 +75,54 @@ class funcoes():
 
             self.conectar.commit()
             self.desconectar_ao_bd()
-            self.monstrar_na_tabela()
+            self.mostrar_na_tabela()
             self.limpar_caixa()
 
-    def monstrar_na_tabela(self):
+    # Função que pega os dados no banco de dados e mostra na tabela
+    def mostrar_na_tabela(self):
+            # Delete os dados da tabela antes para não trazer informação repetida
             self.tabela.delete(*self.tabela.get_children())
             self.conectar_ao_bd()
+
+            # Seleciona os dados no banco
             dados = self.cursor.execute("""
                 select id, Nome_contato, telefone_um, telefone_dois, telefone_tres from contato
                 order by Nome_contato asc; 
                 """)
 
+            # Inseri os daados na tabela
             for i in dados:
                     self.tabela.insert("", END, values=i)
 
             self.desconectar_ao_bd()
 
+    # 'event' siginifica que ele vai imteragir com outro código no caso da tabela
+    def selecionar(self, event):
+            self.limpar_caixa()
+            self.tabela.selection()
+
+            for t in self.tabela.selection():
+                    col1, col2, col3, col4, col5 = self.tabela.item(t, 'values')
+                    self.codigo_entry.insert(END, col1)
+                    self.nome_entry.insert(END, col2)
+                    self.primeiro_numero.insert(END, col3)
+                    self.segundo_numero.insert(END, col4)
+                    self.terceiro_numero.insert(END, col4)
+
+    # Função que deleta um contato da tabela
+    def deletar(self):
+        self.variaveis()
+        self.conectar_ao_bd()
+
+        self.cursor.execute("""
+        delete from contato where id = ?
+        """,
+        (self.codigo_id))
+
+        self.conectar.commit()
+        self.desconectar_ao_bd()
+        self.limpar_caixa()
+        self.mostrar_na_tabela()
 
 class Application(funcoes):
     def __init__(self):
@@ -122,7 +135,7 @@ class Application(funcoes):
         self.botoes()
         self.tabela_de_contatos()
         self.criar_tabela()
-        self.monstrar_na_tabela()
+        self.mostrar_na_tabela()
         self.janela_principal.mainloop()
 
 
@@ -156,7 +169,7 @@ class Application(funcoes):
         # Titulo da janela na barra de menu
         self.contatos_titulo = Label(self.janela_principal, 
                 text='Agenda Telefônica',
-                fg='white',
+                fg='#FFFFFF',
                 bg='#2F4F4F',
                 font=("Righteous", 20)
                 )
@@ -165,44 +178,52 @@ class Application(funcoes):
         self.nome_label = Label(self.primeiro_frame,
                 text='Nome:',
                 font=('Trebuchet', 15, 'bold'),
-                fg='white',
+                fg='#FFFFFF',
+                bg='#2F4F4F')
+
+        self.codigo_label = Label(self.primeiro_frame,
+                text='Indentificador:',
+                font=('Trebuchet', 15, 'bold'),
+                fg='#FFFFFF',
                 bg='#2F4F4F')
 
         # Titulo da treeview do segundo frame
         self.todos_contatos_label = Label(self.segundo_frame,
                 text='Todos os contatos:',
-                font=('trebuchet', 15, 'bold'),
-                fg='white',
+                font=('Trebuchet', 15, 'bold'),
+                fg='#FFFFFF',
                 bg='#2F4F4F')
 
-        # Titulo dos entry que recebe os números de telefones
-        self.telefones_label = Label(self.primeiro_frame,
-                text='Telefones',
-                font=('trebuched', 15, 'bold'),
-                fg='white',
+        # Titulos dos entry que recebe os números de telefones
+        self.celular_label = Label(self.primeiro_frame,
+                text='Celular:',
+                font=('Trebuchet', 15, 'bold'),
+                fg='#FFFFFF',
+                bg='#2F4F4F')
+
+        self.telefone_um_label = Label(self.primeiro_frame,
+                text='Telefone:',
+                font=('Trebuchet', 15, 'bold'),
+                fg='#FFFFFF',
+                bg='#2F4F4F')
+
+        self.telefone_dois_label = Label(self.primeiro_frame,
+                text='Telefone 2:',
+                font=('Trebuchet', 15, 'bold'),
+                fg='#FFFFFF',
                 bg='#2F4F4F')
         
         # Chamando todas as labels para a janela
         self.contatos_titulo.place(x=10, y=5)
         self.nome_label.place(x=10, y=20)
-        self.telefones_label.place(x=370, y=70)
+        self.codigo_label.place(x=450, y=20)
+        self.celular_label.place(x=20, y=80)
+        self.telefone_um_label.place(x=290, y=80)
+        self.telefone_dois_label.place(x=560, y=80)
         self.todos_contatos_label.place(x=10, y=20)
 
 
     def caixas_de_texto(self):
-        self.codigo_entry = Entry(self.primeiro_frame,
-                font=('Times', 15),
-                fg='#000000',
-                borderwidth=2,
-                relief="raised"
-                )
-        #self.codigo_entry.place(x=550, y=20, width=100)
-        # Função que apaga a mensagem de dita da caixa de texto
-        def on_click(event):
-            self.caixa_pesquisa.configure(state=NORMAL)
-            self.caixa_pesquisa.delete(0, END)
-
-            self.caixa_pesquisa.unbind('<Button-1>', on_click)
 
         # Caixa de pesquisa da barra de menu
         self.caixa_pesquisa = Entry(self.janela_principal,
@@ -211,75 +232,49 @@ class Application(funcoes):
                 borderwidth=2,
                 relief="raised"
                 )
-        self.caixa_pesquisa.insert(0, "Buscar...")
-        self.caixa_pesquisa.configure(state=DISABLED)
-        on_click_id = self.caixa_pesquisa.bind('<Button-1>', on_click)
 
         # Caixa de texto que recebe o nome
-        def on_click(event):
-            self.nome_entry.configure(state=NORMAL)
-            self.nome_entry.delete(0, END)
-
-            self.nome_entry.unbind('<Button-1>', on_click)
         self.nome_entry = Entry(self.primeiro_frame,
                 font=('Times', 15),
                 fg='#000000',
                 borderwidth=2,
                 relief='raised')
 
+        # Caixa que monstra o código do contato
+        self.codigo_entry = Entry(self.primeiro_frame,
+                font=('Times', 15),
+                fg='#000000',
+                borderwidth=2,
+                relief="raised"
+                )
+
         # Adicionando as três caixas de texto que receberão os números de telefone
 
         ### RECEBE O PRIMEIRO NUMERO ###
-        def on_click(event):
-            self.primeiro_numero.configure(state=NORMAL)
-            self.primeiro_numero.delete(0, END)
-
-            self.primeiro_numero.unbind('<Button-1>', on_click)
-
         self.primeiro_numero = Entry(self.primeiro_frame,
                 font=('Times', 15),
                 fg='#000000',
                 borderwidth=2,
                 relief='raised')
-        self.primeiro_numero.insert(0, "Número 1")
-        self.primeiro_numero.configure(state=DISABLED)
-        on_click_id_2 = self.primeiro_numero.bind('<Button-1>', on_click)
 
         ### RECEBE O SEGUNDO NUMERO ###
-        def on_click(event):
-            self.segundo_numero.configure(state=NORMAL)
-            self.segundo_numero.delete(0, END)
-
-            self.segundo_numero.unbind('<Button-1>', on_click)
-
         self.segundo_numero = Entry(self.primeiro_frame,
                 font=('Times', 15),
                 fg='#000000',
                 borderwidth=2,
                 relief='raised')
-        self.segundo_numero.insert(0, "Número 2")
-        self.segundo_numero.configure(state=DISABLED)
-        on_click_id_3 = self.segundo_numero.bind('<Button-1>', on_click)
 
         ### RECEBE O TERCEIRO NUMERO ###
-        def on_click(event):
-            self.terceiro_numero.configure(state=NORMAL)
-            self.terceiro_numero.delete(0, END)
-
-            self.terceiro_numero.unbind('<Button-1>', on_click)
-
         self.terceiro_numero = Entry(self.primeiro_frame,
                 font=('Times', 15),
                 fg='#000000',
                 borderwidth=2,
                 relief='raised')
-        self.terceiro_numero.insert(0, "Número 3")
-        self.terceiro_numero.configure(state=DISABLED)
-        on_click_id_4 = self.terceiro_numero.bind('<Button-1>', on_click)
 
         # Chamando as caxas de texto
         self.caixa_pesquisa.place(x=600, y=12)
-        self.nome_entry.place(x=80, y=20, width=300)
+        self.nome_entry.place(x=80, y=20, width=300)        
+        self.codigo_entry.place(x=600, y=20, width=100)
         self.primeiro_numero.place(x=20, y=110, width=250)
         self.segundo_numero.place(x=290, y=110, width=250)
         self.terceiro_numero.place(x=560, y=110, width=250)
@@ -298,21 +293,22 @@ class Application(funcoes):
                 command=self.adicionar_cliente,
                 text='Salvar',
                 font=('verdana', 10, 'bold'),
-                fg='white',
+                fg='#FFFFFF',
                 bg='#5271FF')
 
         # Botão de editar contato
         self.editar_button = Button(self.primeiro_frame,
                 text='Editar',
                 font=('verdana', 10, 'bold'),
-                fg='white',
+                fg='#FFFFFF',
                 bg='#5271FF')
 
         # Botão remover contatos
         self.remover_button = Button(self.segundo_frame,
+                command=self.deletar,
                 text='Remover',
                 font=('verdana', 10, 'bold'),
-                fg='white',
+                fg='#FFFFFF',
                 bg='#FF5757')
 
         # Botão para sair da janela
@@ -320,7 +316,7 @@ class Application(funcoes):
                 command=quit,
                 text='Sair',
                 font=('verdana', 10, 'bold'),
-                fg='white',
+                fg='#FFFFFF',
                 bg='#FF5757')
 
         # Botão para limpar as caixas de texto
@@ -328,7 +324,7 @@ class Application(funcoes):
                 command=self.limpar_caixa,
                 text='Limpar',
                 font=('verdana', 10, 'bold'),
-                fg='white',
+                fg='#FFFFFF',
                 bg='#FF5757')
         
         # Chamando os botões na tela
@@ -361,6 +357,9 @@ class Application(funcoes):
         self.tabela.column('#3', width=180, minwidth=180)
         self.tabela.column('#4', width=180, minwidth=180)
         self.tabela.column('#5', width=180, minwidth=180)
+
+        # Criando a função que selecionar a coluna na tabela ('.bind' é a que faz interação com a tabela)
+        self.tabela.bind("<Double-1>", self.selecionar)
 
         # Especificando o tamanho no frame
         self.tabela.place(x=15, y=50, width=800, height=180)
